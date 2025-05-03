7ace114874a55be4b6beb44d80941fe1bdd5b357
fikertag <149786264+fikertag@users.noreply.github.com>
2025-05-03 10:27:09 +0300

    vercel tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { MessageProvider } from "@/context/Message";
+import { Analytics } from "@vercel/analytics/next";
 
 import { Header } from "@/components/header";
 import type { ReactNode } from "react";
@@ -48,6 +49,7 @@ export default function RootLayout({
               <Header />
 
               {children}
+              <Analytics />
             </div>
           </MessageProvider>
         </Providers>
